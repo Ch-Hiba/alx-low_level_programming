3d2ecfbf36e1a54161b1a01da911556f9f69d1da
Ch-Hiba <choklatihibagmail.com>
2023-02-16 15:54:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - print exactly
+ * main - main block
  * Return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful- Dora Korpar, 2015-10-19\n");
-	write(2
-	      "and that piece of art is useful- Dora Korpar, 2015-10-19\n",
-	      s);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
