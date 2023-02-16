8c5217d63eed16bff6cc321cda7ae29f64d75195
Ch-Hiba <choklatihibagmail.com>
2023-02-16 12:10:32 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h
+/**
+ * main - usinf puts fonction
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
