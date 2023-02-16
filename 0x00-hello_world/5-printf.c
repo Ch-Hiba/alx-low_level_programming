9be754fda4fc76e84c224fa873a5b94b52bd942e
Ch-Hiba <choklatihibagmail.com>
2023-02-16 12:48:14 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a line using printf fonction
- * Return: 0 (Success)
+ * main - using print f 
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
