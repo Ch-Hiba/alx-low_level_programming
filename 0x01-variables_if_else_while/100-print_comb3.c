b90f142b65bb02975529a5952ba0d67c038be25c
Ch-Hiba <choklatihibagmail.com>
2023-02-17 13:48:36 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,12 @@ int main(void)
 		j = i / 10;
 		k = i % 10;
 
-		putchar(j + '0\n');
+		putchar(j + '0');
 		putchar(k + '0');
 
 		if (i < 99)
 		{
-			putchar(44);
+			putchar(44\n);
 			putchar(32);
 		}
 	}
