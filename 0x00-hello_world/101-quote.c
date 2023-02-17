894c4ffde84441789de6da169eda299947a8935e
Ch-Hiba <choklatihibagmail.com>
2023-02-16 17:07:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\"
-		 - Dora Korpar, 2015-10-19\n");
+	int s;
 
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
