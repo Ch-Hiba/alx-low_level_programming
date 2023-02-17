23de876122d2e1f7ca599ea7488ec0cd74d84e26
Ch-Hiba <choklatihibagmail.com>
2023-02-16 16:32:14 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
  * main - print a line to the standard
@@ -8,6 +7,5 @@
 int main(void)
 {
 	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
