d9a55a3f8be90184f0a4581092bdab3d87b1eb99
Ch-Hiba <choklatihibagmail.com>
2023-02-17 13:20:46 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print numbers from 0 to 99.
- * Numbers must be separated by commas followed by a space.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use variables of type `char`.
- * Return: 0
+ * Description: Print numbers from 0 to 99
+ * Return:0
  */
 int main(void)
 {
