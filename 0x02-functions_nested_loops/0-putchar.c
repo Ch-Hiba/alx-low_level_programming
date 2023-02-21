1db0acafa61e2899dbdafb0fd731dd7731c66870
Ch-Hiba <choklatihibagmail.com>
2023-02-21 12:25:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,12 @@
  */
 int main(void)
 {
-	char abe[] = "_putchar";
+	char [H] = "_putchar";
 
 	int c;
-
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(abe[c]);
+		_putchar(H[c]);
 	}
 	_putchar('\n');
 	return (0);
-}
