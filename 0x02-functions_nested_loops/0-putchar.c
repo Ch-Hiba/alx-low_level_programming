2ec836af9299dbebff69c18455c7fcc99465ac31
Ch-Hiba <choklatihibagmail.com>
2023-02-21 11:41:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * main - Entry begins
  * Return: nothing, return void
@@ -8,6 +7,20 @@
 
 int main(void)
 {
-printf:('_putchar', '\n')
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
+
 	       return (0);
 }
