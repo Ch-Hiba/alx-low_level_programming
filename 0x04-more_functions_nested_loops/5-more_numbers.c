75872e0eac17076d06f002067eb18d796a0b6df1
Ch-Hiba <choklatihibagmail.com>
2023-02-23 13:27:34 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * prints 10 times the numbers, from 0 to 14
+ *
+ */
+void more_numbers(void);
+{
+	int c, i;
+	c=0
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{	
+			if (i < 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			i++;
+		}
+	c++;
+	_putchar('/n');
+	}
+}
