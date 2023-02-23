f042829d0c6afbd5d717005dd9854294535da99f
Ch-Hiba <choklatihibagmail.com>
2023-02-23 12:03:10 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *Function prototypes
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+
+
+
+
+
+
+
+#endif /* MAIN_H */
