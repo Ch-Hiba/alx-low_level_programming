fd30f4f4468f926ea191e6db4c4dd99402a0a27c
Ch-Hiba <choklatihibagmail.com>
2023-02-23 14:18:54 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,54 +1,66 @@
+#include <stdio.h>
+#include <limits.h>
 #include "main.h"
-#include "stdio.h"
+
+int _pow(int x, int y);
 
 /**
  * print_number - prints an integer
- * @n: the integer to print
+ *
+ * @num: number to print
+ *
  */
-void print_number(int n)
+void print_number(int num)
 {
-	int i;
-	int max;
-	int minVal = n + 1 == -2147483640 - 7 ? -1 : 0;
-	int rem = n < 0 ? 0 - n + minVal : n;
+	int quot, degree, i, is_int_min, temp;
 
-	max = 1;
-	max *= 100000;
-	max *= 100000;
-
-	if (n < 0)
-		_putchar('-');
-
-	for (i = 9; i >= 0; i--)
+	is_int_min = 0;
+	if (num == INT_MIN)
 	{
-		int power = b10_pow(i);
-
-		if (rem > power - 1 && !(i == 0 && minVal < 0))
-			_putchar((int)((rem / power) % 10) + '0');
-		else if (i == 0 && minVal >= 0)
-			_putchar((int)((rem / power) % 10) + '0');
-		if (i == 0 && minVal < 0)
-			_putchar('8');
+		num++;
+		is_int_min = 1;
+	}
+	if (num < 0)
+	{
+		num = -num;
+		_putchar('-');
+	}
+	quot = num;
+	degree = 0;
+	while (quot / 10 > 0)
+	{
+		++degree;
+		quot /= 10;
+	}
+	quot = num;
+	for (i = degree; i >= 0; --i)
+	{
+		temp = quot / _pow(10, i);
+		if (i == 0 && is_int_min)
+			temp += 1;
+		_putchar('0' + temp);
+		quot %= _pow(10, i);
 	}
 }
 
 /**
- * b10_pow - Computes the power of 10 to the given index
- * @idx: The index to which 10 is raised to.
+ * _pow - compute power
  *
- * Return: A power of 10
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: power of the base to the exponent
+ *
  */
-int b10_pow(int idx)
+int _pow(int x, int y)
 {
-	int i;
 	int result = 1;
 
-	if (idx < 0)
-		return (0);
-
-	for (i = 0; i < idx; i++)
+	while (y >= 1)
 	{
-		result *= 10;
+		result *= x;
+		--y;
 	}
+
 	return (result);
 }
