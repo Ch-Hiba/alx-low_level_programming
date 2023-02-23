65c41409300bd049949114532f0e87c71fd902ea
Ch-Hiba <choklatihibagmail.com>
2023-02-23 14:13:45 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,54 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
- * print_number - print any integer using putchar
- * @n: integer to be printed
+ * print_number - prints an integer
+ * @n: the integer to print
  */
-
 void print_number(int n)
 {
-	int count, x, y, k;
+	int i;
+	int max;
+	int minVal = n + 1 == -2147483640 - 7 ? -1 : 0;
+	int rem = n < 0 ? 0 - n + minVal : n;
 
-	x = 0;
-	y = 1000000000;
+	max = 1;
+	max *= 100000;
+	max *= 100000;
 
-	if (n == 0)
-		_putchar('0');
-	else if (n > 0)
-		n *= -1;
-	else
-	       	_putchar('-');
-	for (count = 0; count < 10; count++)
+	if (n < 0)
+		_putchar('-');
+
+	for (i = 9; i >= 0; i--)
 	{
-		if (n / y == 0 && x == 0)
-		{
-			y /= 10;
-			
-			continue;
-		}
-		else if (x == 0)
-		{
-			_putchar(-(n / y) + '0');
-			x += 1;
-		}
-		else
-		{
-			k = (-(n / y) % 10);
-			if (k < 0)
-				k *= -1;
-			_putchar(k + '0');
-		}
-		y /= 10;
+		int power = b10_pow(i);
+
+		if (rem > power - 1 && !(i == 0 && minVal < 0))
+			_putchar((int)((rem / power) % 10) + '0');
+		else if (i == 0 && minVal >= 0)
+			_putchar((int)((rem / power) % 10) + '0');
+		if (i == 0 && minVal < 0)
+			_putchar('8');
 	}
+}
+
+/**
+ * b10_pow - Computes the power of 10 to the given index
+ * @idx: The index to which 10 is raised to.
+ *
+ * Return: A power of 10
+ */
+int b10_pow(int idx)
+{
+	int i;
+	int result = 1;
+
+	if (idx < 0)
+		return (0);
+
+	for (i = 0; i < idx; i++)
+	{
+		result *= 10;
+	}
+	return (result);
 }
