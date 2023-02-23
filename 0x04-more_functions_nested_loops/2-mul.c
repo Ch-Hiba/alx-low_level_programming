c7009e2b975d2bfa50def731acfb275d61bb38b2
Ch-Hiba <choklatihibagmail.com>
2023-02-23 12:34:07 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul -> Mult two int
+ *
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: N of a, b
+ */
+int mul(int a, int b)
+{
+	int N = a * b;
+
+
+	return (N);
+}
