baf3e864f960eb8dd3e4dbc77c897038851d81a4
Ch-Hiba <choklatihibagmail.com>
2023-02-27 10:28:55 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 /**
  * reset_to_98 - Set pointer param to 98.
  * @n: int type pointer
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_C
-#define MAIN_C
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
