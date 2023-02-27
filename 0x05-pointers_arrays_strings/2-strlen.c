88f3e2bfc0c594f41de7a96e263265239be418a0
Ch-Hiba <choklatihibagmail.com>
2023-02-27 12:09:47 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "mai.h"
+
+/**
+ * _strlen - return the length of a string
+ * @str: the string to be processed
+ * Return: the length of the string
+ */
+int  _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	return (i);
+}
