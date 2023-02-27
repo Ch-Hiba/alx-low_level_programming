21ea30542bc92bd7eade8fea375057fa0c08d50a
Ch-Hiba <choklatihibagmail.com>
2023-02-27 12:15:45 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
-/**
- * print_rev - prints a string in reverse
- * @s: char array string type
- * description: can only use _putchar
- */
-void print_rev(char *s)
-{
-	int i;
 
-	for (i--; s[i] >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
+/**
+ * print_rev - print a string in reverse
+ * @str: string to be processed
+ */
+void print_rev(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	for (j = i - 1; j >= 0; --j)
+		_putchar(str[j]);
 	_putchar('\n');
 }
