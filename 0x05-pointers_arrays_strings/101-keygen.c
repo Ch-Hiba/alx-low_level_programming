07d98a29b83575754e84d67afd26b55ffd85164c
Ch-Hiba <choklatihibagmail.com>
2023-02-27 12:52:33 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: my keygen.
+ * Return: 0.
+ */
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	time_t t;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	srand((unsigned int) time(&t));
+	while (j < 2772)
+	{
+		i = rand() % 128;
+		if ((j + i) > 2772)
+			break;
+		j += i;
+		k++;
+		printf("%c", i);
+	}
+	printf("%c\n", (2772 - j));
+	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-void print_number(int n);
