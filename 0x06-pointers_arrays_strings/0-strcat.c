be6e30670557dd6dd9a0693d6a0b172e8b0e7370
Ch-Hiba <choklatihibagmail.com>
2023-03-02 09:47:35 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* *_strcat - a function that concatenates two strings
+* Return: string
+**/
+
+char *_strcat(char *dest, char *src)
+{
+	int l;
+	int i;
+
+	for (l = 0; dest[l] != '\0'; l++)
+	{
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[l + i] = src[i];
+	}
+	return (dest);
+}
