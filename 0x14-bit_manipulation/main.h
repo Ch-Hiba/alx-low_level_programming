ae666da6a0a02cfe3bd1446be1be4c5b1d921513
Ch-Hiba <choklatihiba@gmail.com>
2023-05-04 17:48:04 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,29 +1,13 @@
-#ifndef H
-#define H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-unsigned int binary_to_uint(const char *);
-
-int _putchar(char );
-
-void print_binary(unsigned long int);
-
-int get_bit(unsigned long int, unsigned int);
-
-int set_bit(unsigned long int *, unsigned int);
-
-void convert_to_binary(unsigned long int, unsigned short *);
-
-unsigned long int convert_to_decimal(unsigned short *);
-
-int clear_bit(unsigned long int *, unsigned int);
-
-void convert_to_binary2(unsigned long int, unsigned short *);
-
-unsigned long int convert_to_decimal2(unsigned short *);
-
-unsigned int flip_bits(unsigned long int , unsigned long int);
-
-void convert_to_binary3(unsigned long int, unsigned short *);
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* H */
+#endif
